f946b3ffb78e14416fc010f992611ffb21c3b7f5
xiaonan-w <1751431387@qq.com>
2021-07-14 08:12:49 +0800

    王肖楠提交员工管理界面添加员工表单

--- src/views/Admin/AdminManagement.vue
+++ src/views/Admin/AdminManagement.vue
@@ -39,52 +39,23 @@
     <el-dialog
         title="添加员工"
         :visible.sync="dialogVisible"
-        width="30%">
+        width="60%">
       <span>
         <el-form ref="form" :model="addForm" label-width="80px">
-  <el-form-item label="活动名称">
-    <el-input v-model="addForm.name"></el-input>
-  </el-form-item>
-  <el-form-item label="活动区域">
-    <el-select v-model="form.region" placeholder="请选择活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="活动时间">
-    <el-col :span="11">
-      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="即时配送">
-    <el-switch v-model="form.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="活动性质">
-    <el-checkbox-group v-model="form.type">
-      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-      <el-checkbox label="地推活动" name="type"></el-checkbox>
-      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="特殊资源">
-    <el-radio-group v-model="form.resource">
-      <el-radio label="线上品牌商赞助"></el-radio>
-      <el-radio label="线下场地免费"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="活动形式">
-    <el-input type="textarea" v-model="form.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
-</el-form>
+          <el-form-item label="员工姓名">
+            <el-input v-model="addForm.name"></el-input>
+          </el-form-item>
+            <el-form-item label="员工手机号">
+          <el-input v-model="addForm.phone"></el-input>
+            </el-form-item>
+          <el-form-item label="员工职位">
+            <el-radio-group v-model="addForm.staffTpye">
+              <el-radio label="前台"></el-radio>
+              <el-radio label="服务员"></el-radio>
+              <el-radio label="厨师"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
       </span>
       <span slot="footer" class="dialog-footer">
     <el-button @click="addStaffCancel">取 消</el-button>
@@ -143,6 +114,11 @@ export default {
         id: '',
         name: '',
         staffType:''
+      },
+      addForm:{
+        name: '',
+        phone:'',
+        staffTpye:''
       },
       tableData:[{
         id:'',
