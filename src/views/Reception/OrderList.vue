10d86d515c1e53995128ee646be33aa107b8d72c
Libiao <1194714301@qq.com>
2021-07-29 15:19:22 +0800

    前后端对接1.4 前台代付款订单

--- src/views/Reception/OrderList.vue
+++ src/views/Reception/OrderList.vue
@@ -30,6 +30,12 @@
               <el-button type="text" @click="getorderList(scope.row.orderId);dialogorderList= true">详情</el-button>
         </template>
       </el-table-column>
+      <el-table-column
+          label="收款">
+        <template slot-scope="scope">
+          <el-button type="success" @click="updateOrderState(scope.row.orderId)">确认付款</el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
       <el-dialog
@@ -42,7 +48,7 @@
             style="width: 100%"
             :header-cell-style="{background:'#eef1f6',color:'#606266'}">
           <el-table-column
-              prop="dishId"
+              prop="dishName"
               label="菜品名称"
               width="180">
           </el-table-column>
@@ -57,14 +63,15 @@
               width="100">
           </el-table-column>
         </el-table>
-        <el-button type="warning" plain>确认付款</el-button>
+        <el-button type="warning" @click="toPay();dialogorderList= true">付款</el-button>
       </el-dialog>
   </div>
 </template>
 
 <script>
 import ReceptionHeader from "../../components/ReceptionHeader";
-
+import qs from "qs";
+import api from "../../util/api.js"
 
 export default {
   components: {ReceptionHeader},
@@ -72,6 +79,7 @@ export default {
   name: "OrderList",
   data() {
     return {
+      orderid:'',
       dialogorderList: false,
       spanArr:[],
       order:[{
@@ -83,19 +91,47 @@ export default {
       }],
       orderList:[{
         listId:'',
-        dishId: '',
+        orderId:'',
+        dishName: '',
         dishNum: '',
         dishPrice: '',
         listStatus: ''
-      }]
+      }],
+      backData:'',
     }
   },
   methods:{
     getorderList(orderId){
-      var path = "/Data/orderList.json"
-      this.axios.get(path).then((response)=>{
+      var that =this
+      that.orderid=orderId
+      let path = api.path+"/order/orderlist/showOrderInfo"
+      this.axios.post(path,qs.stringify({"orderId":orderId})).then((response) => {
         console.log(response)
-        this.orderList=response.data.data
+        this.orderList = response.data.data
+      })
+    },
+    updateOrderState(orderId){
+      var that =this
+      that.orderid=orderId
+      let path = api.path+"/order/order/updateOrderState"
+      this.axios.post(path,qs.stringify({"orderId":orderId})).then((response) => {
+        console.log(response)
+        location.reload()
+      })
+    },
+
+    toPay(){
+      let path = api.path+"/order/order/toPay"
+      this.axios.post(path,qs.stringify({"orderId":this.orderid})).then((response) => {
+        console.log(response)
+        this.backData=response.data;
+        console.log(this.backData);
+        document.write('<html>' +
+            '<head></head>' +
+            '<body>' +
+            this.backData +
+            '</body>' +
+            '</html>')
       })
     },
 
@@ -108,7 +144,7 @@ export default {
     }
   },
   mounted() {
-    var path = "/Data/order.json"
+    let path = api.path+"/order/order/showOrder"
     this.axios.get(path).then((response)=>{
       this.order=response.data.data
     })
